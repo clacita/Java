c1aadb4e7c6b0f5fc706db1cc6050fe627e15021
claci <claci_aponte@hotmail.com>
2023-02-06 17:41:55 -0500

    claci

---
+++ 1-MyFirstProgram/src/Claci.java
@@ -0,0 +1,9 @@
+public class Claci {
+    public static void main(String[] args) {
+        int a=5, b=10;
+        if (a>b){
+            System.out.println("Both are not equal");
+        }
+
+    }
+}
